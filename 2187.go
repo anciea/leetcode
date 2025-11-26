0aba03e059a7d1ca8cf3fe7e2d9c69678ec5774a
maqianli <liuxinqi@xiaozujian.com>
2025-11-26 16:25:13 +0800

    refactor: comment out solution for problem 2187 to resolve package conflicts

--- 2187.go
+++ 2187.go
@@ -1,22 +1,22 @@
 package main
 
-func minimumTime(time []int, totalTrips int) int64 {
-	l, r := 1, int(1e9)
-	for l < r {
-		m := (l + r) >> 1
-		if check(time, totalTrips, m) {
-			r = m
-		} else {
-			l = m + 1
-		}
-	}
-	return int64(l)
-}
+// func minimumTime(time []int, totalTrips int) int64 {
+// 	l, r := 1, int(1e9)
+// 	for l < r {
+// 		m := (l + r) >> 1
+// 		if check(time, totalTrips, m) {
+// 			r = m
+// 		} else {
+// 			l = m + 1
+// 		}
+// 	}
+// 	return int64(l)
+// }
 
-func check(time []int, totalTrips int, m int) bool {
-	sum := 0
-	for _, x := range time {
-		sum += m / x
-	}
-	return sum >= totalTrips
-}
+// func check(time []int, totalTrips int, m int) bool {
+// 	sum := 0
+// 	for _, x := range time {
+// 		sum += m / x
+// 	}
+// 	return sum >= totalTrips
+// }
