fc62be92f7c5d08ff939382465e6cdffdc7038d8
maqianli <liuxinqi@xiaozujian.com>
2025-12-03 11:23:39 +0800

    refactor: comment out initial solution for problem 3623 - count trapezoids
    - Comment out countTrapezoids implementation using y-coordinate grouping approach
    - Preserve original solution that groups points by y-level and calculates edge combinations
    - Keep frequency map method and cumulative sum logic for potential future reference

--- 3623.go
+++ 3623.go
@@ -1,17 +1,17 @@
 package main
 
-func countTrapezoids(points [][]int) int {
-	pointNum := make(map[int]int)
-	mod := 1000000007
+// func countTrapezoids(points [][]int) int {
+// 	pointNum := make(map[int]int)
+// 	mod := 1000000007
 
-	for _, point := range points {
-		pointNum[point[1]]++
-	}
-	ans, sum := 0, 0
-	for _, num := range pointNum {
-		edge := num * (num - 1) / 2
-		ans = (ans + sum * edge) % mod
-		sum = (sum + edge) % mod
-	}
-	return ans
-}
+// 	for _, point := range points {
+// 		pointNum[point[1]]++
+// 	}
+// 	ans, sum := 0, 0
+// 	for _, num := range pointNum {
+// 		edge := num * (num - 1) / 2
+// 		ans = (ans + sum * edge) % mod
+// 		sum = (sum + edge) % mod
+// 	}
+// 	return ans
+// }
