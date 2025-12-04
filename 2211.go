37f9611683a85011292a358052d7b4b4690e3a96
maqianli <liuxinqi@xiaozujian.com>
2025-12-04 11:48:01 +0800

    feat: add solution for problem 2211 - count collisions on a road
    - Implement countCollisions by removing cars that won't collide from both ends
    - Trim left-moving cars from prefix as they move away from collision zone
    - Trim right-moving cars from suffix as they move away from collision zone
    - Calculate collisions as remaining cars minus stationary ones (all non-stationary cars in middle will collide)

---
+++ 2211.go
@@ -0,0 +1,9 @@
+package main
+
+import "strings"
+
+func countCollisions(s string) int {
+	s = strings.TrimLeft(s, "L")          // 前缀向左的车不会发生碰撞
+	s = strings.TrimRight(s, "R")         // 后缀向右的车不会发生碰撞
+	return len(s) - strings.Count(s, "S") // 剩下非静止的车必然会碰撞
+}
