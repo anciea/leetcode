2c3765d98657cfe7203001ae0357a5e2b5d7402d
maqianli <1014696466@qq.com>
2025-12-23 18:04:20 +0800

    feat: add solutions for problems 1751 and 2054 - maximum value from attending events
    - Implement maxValue (1751) using dynamic programming to find maximum value from attending k events
    - Sort events by end time to enable binary search for non-overlapping events
    - Use 2D DP array dp[i][j] where i is event index and j is number of events attended
    - For each event, binary search for latest non-overlapping previous event using end time
    - Choose max of skipping current event or attending it plus optimal

--- 1802.go
+++ 1802.go
@@ -1,33 +1,33 @@
 package main
 
-func maxValue(n int, index int, maxSum int) int {
-	l,r := 0,maxSum+1
-	for l+1 < r {
-		m := l + (r-l)/2
-		if checkMaxValue(n,index,maxSum,m) {
-			l = m
-		} else {
-			r = m
-		}
-	}
-	return l
-}
+// func maxValue(n int, index int, maxSum int) int {
+// 	l,r := 0,maxSum+1
+// 	for l+1 < r {
+// 		m := l + (r-l)/2
+// 		if checkMaxValue(n,index,maxSum,m) {
+// 			l = m
+// 		} else {
+// 			r = m
+// 		}
+// 	}
+// 	return l
+// }
 
 
-func checkMaxValue(n int, index int, maxSum int, m int) bool {
-	left := segmentSum(m-1,index)
-	right := segmentSum(m-1,n-index-1)
-	return left + right + m <= maxSum
-}
+// func checkMaxValue(n int, index int, maxSum int, m int) bool {
+// 	left := segmentSum(m-1,index)
+// 	right := segmentSum(m-1,n-index-1)
+// 	return left + right + m <= maxSum
+// }
 
-func segmentSum (n, l int) int{
-	ans := 0
-	if l >= n {
-		ans += ((1+n)*n)/2
-		l -= n
-		ans += l
-	} else {
-		ans += ((2*n-l+1)*l)/2
-	}
-	return ans
-}
+// func segmentSum (n, l int) int{
+// 	ans := 0
+// 	if l >= n {
+// 		ans += ((1+n)*n)/2
+// 		l -= n
+// 		ans += l
+// 	} else {
+// 		ans += ((2*n-l+1)*l)/2
+// 	}
+// 	return ans
+// }
