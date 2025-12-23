d5dac11311b28341cf2cf4123d691ff8dc0a0deb
maqianli <liuxinqi@xiaozujian.com>
2025-12-23 18:04:20 +0800

    feat: add solutions for problems 1751 and 2054 - maximum value from attending events
    - Implement maxValue (1751) using dynamic programming to find maximum value from attending k events
    - Sort events by end time to enable binary search for non-overlapping events
    - Use 2D DP array dp[i][j] where i is event index and j is number of events attended
    - For each event, binary search for latest non-overlapping previous event using end time
    - Choose max of skipping current event or attending it plus optimal

---
+++ 2054.go
@@ -0,0 +1,31 @@
+package main
+
+import "sort"
+
+func maxTwoEvents(events [][]int) (ans int) {
+	k := 2
+	sort.Slice(events, func(i, j int) bool {
+		return events[i][1] < events[j][1]
+	})
+
+	n := len(events)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, k+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		st := events[i-1][0]
+		value := events[i-1][2]
+		idx := sort.Search(i-1, func(j int) bool {
+			return events[j][1] >= st
+		})
+
+		for j := 1; j <= k; j++ {
+			dp[i][j] = max(dp[i-1][j], dp[idx][j-1]+value)
+		}
+
+	}
+
+	return dp[n][k]
+}
