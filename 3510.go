99bc1d2a36178c3082dc717fef7a7fd99bce9cbb
maqianli <liuxinqi@xiaozujian.com>
2026-01-26 16:28:16 +0800

    refactor: simplify variable naming and formatting in problem 3510 solution
    - Rename comparison function parameters from (a, b) to (x, y) for consistency
    - Change 'pre' to 'prev' for clearer previous element reference
    - Remove Chinese comments explaining data structures and logic
    - Standardize spacing around operators and function calls
    - Fix idx.Remove to remove index i instead of nxt after merging pairs
    - Maintain same algorithmic logic while improving code readability

--- 3510.go
+++ 3510.go
@@ -8,68 +8,63 @@ import (
 func minimumPairRemoval(nums []int) (ans int) {
 	n := len(nums)
 	type pair struct{ s, i int }
-	// (相邻元素和，左边那个数的下标)
-	pairs := redblacktree.NewWith[pair, struct{}](func(a, b pair) int { return cmp.Or(a.s-b.s, a.i-b.i) })
-	dec := 0 // 递减的相邻对的个数
-	for i := range n - 1 {
-		x, y := nums[i], nums[i+1]
+	pairs := redblacktree.NewWith[pair, struct{}](func(x, y pair) int {return cmp.Or(x.s-y.s, x.i-y.i)})
+	dec := 0
+
+	for i := range n-1 {
+		x,y := nums[i], nums[i+1]
 		if x > y {
 			dec++
 		}
-		pairs.Put(pair{x + y, i}, struct{}{})
+		pairs.Put(pair{x+y, i}, struct{}{})
 	}
 
-	// 剩余下标
 	idx := redblacktree.New[int, struct{}]()
 	for i := range n {
 		idx.Put(i, struct{}{})
 	}
-
 	for dec > 0 {
 		ans++
 
 		it := pairs.Left()
 		s := it.Key.s
 		i := it.Key.i
-		pairs.Remove(it.Key) // 删除相邻元素和最小的一对
-
-		// (当前元素，下一个数)
-		node, _ := idx.Ceiling(i + 1)
+		pairs.Remove(it.Key)
+		
+		node, _ := idx.Ceiling(i+1)
 		nxt := node.Key
-		if nums[i] > nums[nxt] { // 旧数据
+		if nums[i] > nums[nxt] {
 			dec--
 		}
 
-		// (前一个数，当前元素)
-		node, _ = idx.Floor(i - 1)
+		node, _ = idx.Floor(i-1)
 		if node != nil {
-			pre := node.Key
-			if nums[pre] > nums[i] { // 旧数据
+			prev := node.Key
+			if nums[prev] > nums[i] {
 				dec--
 			}
-			if nums[pre] > s { // 新数据
+			if nums[prev] > s {
 				dec++
 			}
-			pairs.Remove(pair{nums[pre] + nums[i], pre})
-			pairs.Put(pair{nums[pre] + s, pre}, struct{}{})
+			pairs.Remove(pair{nums[prev] + nums[i], prev})
+			pairs.Put(pair{nums[prev] + s, prev}, struct{}{})
 		}
 
-		// (下一个数，下下一个数)
-		node, _ = idx.Ceiling(nxt + 1)
+		node, _ = idx.Ceiling(nxt+1)
 		if node != nil {
 			nxt2 := node.Key
-			if nums[nxt] > nums[nxt2] { // 旧数据
+			if nums[nxt] > nums[nxt2] {
 				dec--
 			}
-			if s > nums[nxt2] { // 新数据（当前元素，下下一个数）
+			if s > nums[nxt2] {
 				dec++
 			}
 			pairs.Remove(pair{nums[nxt] + nums[nxt2], nxt})
 			pairs.Put(pair{s + nums[nxt2], i}, struct{}{})
 		}
 
-		nums[i] = s // 把 nums[nxt] 加到 nums[i] 中
-		idx.Remove(nxt) // 删除 nxt
+		nums[i] = s
+		idx.Remove(i)
 	}
 	return
 }
