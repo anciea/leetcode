ee7ea47869c3a378ddc2c480cdc20827d52c9544
maqianli <1014696466@qq.com>
2026-04-01 21:31:27 +0800

    feat: add solution for problem 2751 - robot collision simulation with health tracking
    - Implement survivedRobotsHealths using stack-based collision detection for robots moving in opposite directions
    - Sort robot indices by position to process collisions in spatial order
    - Use stack to track rightward-moving robots waiting for potential collisions
    - Process leftward-moving robots by resolving collisions with stack top until health depletes
    - Reduce health of stronger robot by 1 and eliminate weaker robot in each collision
    - When

---
+++ 2751.go
@@ -0,0 +1,46 @@
+package main
+
+import "slices"
+
+func survivedRobotsHealths(positions []int, healths []int, directions string) []int {
+	vis := make([]int, len(positions))
+	for i := range vis {
+		vis[i] = i
+	}
+
+	slices.SortFunc(vis, func(i, j int) int { return positions[i] - positions[j] })
+	stack := make([]int, 0)
+
+	for _, v := range vis {
+		// no.v 号机器人
+		if directions[v] == 'R' {
+			stack = append(stack, v)
+		} else {
+			for len(stack) > 0 && healths[v] > 0 {
+				top := stack[len(stack)-1]
+				if healths[v] < healths[top] {
+					healths[top]--
+					healths[v] = 0
+				} else if healths[v] > healths[top] {
+					stack = stack[:len(stack)-1]
+					healths[v]--
+					healths[top] = 0
+				} else {
+					stack = stack[:len(stack)-1]
+					healths[v] = 0
+					healths[top] = 0
+				}
+			}
+			stack = append(stack, v)
+		}
+	}
+
+	ans := make([]int, 0)
+	for _, i := range healths {
+		if i > 0 {
+			ans = append(ans, i)
+		}
+	}
+
+	return ans
+}
